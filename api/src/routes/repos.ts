e5ede02f041866414ea04c498e65b626e5fa96ee
kokivasileski <thekoki001@gmail.com>
2022-06-29 10:37:39 -0400

    added data to /repos endpoind

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,24 @@
 import { Router, Request, Response } from 'express';
+import fileJSON from '../../data/repos.json';
+import axios from 'axios';
 
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+
+  const responseData = await axios.get(
+    'https://api.github.com/users/silverorange/repos'
+  );
+
+  const joinedData = fileJSON.concat(responseData.data);
+  const finalData = joinedData.filter((obj) => {
+    return obj.fork === false;
+  });
+
+  res.setHeader('content-type', 'application/json');
+  res.json(finalData);
 });
